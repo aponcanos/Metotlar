3e97952ec53de919abaf2121c3dfb0fb4ea162fd
tete <bayram@genet.com.tr>
2021-01-16 23:42:23 +0300

    Add project files.

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematik
+{
+    class DortIslem
+    {
+        public void Topla(int sayi1, int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine(toplam);
+        }
+
+
+        public void Cikar(int sayi1, int sayi2)
+        {
+
+        }
+
+        public void Carp(int sayi1, int sayi2)
+        {
+
+        }
+
+        public void Bol(int sayi1, int sayi2)
+        {
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //Naming Convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tekbrikler sepete eklendi: " +urun.Adi);
+        }
+
+        public void Ekle2(String urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine("Tekbrikler sepete eklendi: " + urunAdi);
+        }
+    }
+}
